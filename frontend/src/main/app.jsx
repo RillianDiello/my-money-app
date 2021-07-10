5588a648908965bfa0dce062a8954fe38dd6e243
RillianDiello <rilliansf@gmail.com>
2021-07-10 15:05:44 -0400

    Sidebar and Menu

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
 import '../common/template/dependencies'
+import React from 'react'
 
 import Header from '../common/template/header'
+import SideBar from '../common/template/sidebar'
 
 
 export default props => (
     <div className='wrapper'>
         <Header />
+        <SideBar />
     </div>
 )
