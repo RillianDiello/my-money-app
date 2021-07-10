108ceef720317437c3daeff9e19a0d3c4fdee49b
RillianDiello <rilliansf@gmail.com>
2021-07-10 15:04:19 -0400

    Ajust

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import '../common/template/dependencies'
 
+import Header from '../common/template/header'
+
 
 export default props => (
-    <div className='wrapper'>       
-        <h1>App</h1>
+    <div className='wrapper'>
+        <Header />
     </div>
 )
