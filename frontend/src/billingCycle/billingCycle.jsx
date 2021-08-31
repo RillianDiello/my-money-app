847a38712a3269215a6adc618567982350e5dc44
RillianDiello <rilliansf@gmail.com>
2021-08-30 22:48:37 -0400

    Criando conteudo e abas

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -1,8 +1,36 @@
 
-import React from 'react'
+import React, { Component } from 'react'
 
-export default props => (
-    <div>
-        <h1>Ciclo de pagamento</h1>
-    </div>
-)
+import ContentHeader from '../common/template/contentHeader'
+import Content from '../common/template/content'
+
+import Tabs from '../common/tab/tabs'
+import TabsHeader from '../common/tab/tabsHeader'
+import TabsContent from '../common/tab/tabsContent'
+import TabHeader from '../common/tab/tabHeader'
+
+class BillingCycle extends Component {
+    render() {
+        return (
+            <div>
+                <ContentHeader title='Billing Cycles' small='Records' />
+                <Content>
+                    <Tabs>
+                        <TabsHeader>
+                            <TabHeader label='Listar' icon='bars' target='tabList'></TabHeader>
+                            <TabHeader label='Incluir' icon='plus' target='tabCreate'></TabHeader>
+                            <TabHeader label='Alterar' icon='pencil' target='tabUpdate'></TabHeader>
+                            <TabHeader label='Excluir' icon='trash-o' target='tabDelete'></TabHeader>
+                        </TabsHeader>
+
+                        <TabsContent>
+
+                        </TabsContent>
+                    </Tabs>
+                </Content>
+            </div>
+        )
+    }
+}
+
+export default BillingCycle;
