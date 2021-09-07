8019214b9026e7fe19c5a17ac239fa0785ce22a1
RillianDiello <rilliansf@gmail.com>
2021-09-06 21:41:25 -0400

    Mostrar abas e conteudos de abas

--- frontend/src/common/tab/tabHeader.jsx
+++ frontend/src/common/tab/tabHeader.jsx
@@ -3,21 +3,25 @@ import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
 import { selectTab } from './tabActions';
+import If from '../operator/if';
 
 class TabsHeader extends Component {
     render() {
-        const selected = this.props.tab.selected === this.props.target        
+        const selected = this.props.tab.selected === this.props.target
+        const viseble = this.props.tab.visible[this.props.target]
 
         return (
-            <li className={selected ? 'active': ''}>
-                <a href='javascript:;'
-                    data-toggle='tab'
-                    onClick={() => this.props.selectTab(this.props.target)}
-                    data-target={this.props.target}>
-                    <i className={`fa fa-${this.props.icon}`}></i>{this.props.label}
+            <If test={viseble}>
+                <li className={selected ? 'active' : ''}>
+                    <a href='javascript:;'
+                        data-toggle='tab'
+                        onClick={() => this.props.selectTab(this.props.target)}
+                        data-target={this.props.target}>
+                        <i className={`fa fa-${this.props.icon}`}></i>{this.props.label}
 
-                </a>
-            </li>
+                    </a>
+                </li>
+            </If>
         )
     }
 }
