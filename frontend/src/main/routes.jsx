06de5e8efad531fdba579c7c064ed2c9b51b8828
Rillian Diello <rilliansf@gmail.com>
2021-12-17 00:10:16 -0400

    Auth front and and routing

--- frontend/src/main/routes.jsx
+++ frontend/src/main/routes.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
-import { Router, Route, Redirect, hashHistory } from 'react-router'
+import { Router, Route, Redirect, hashHistory, IndexRoute } from 'react-router'
 
 // import Dashboard from '../dashboard/dashboard'
+import AuthOrApp from './authOrApp'
 import Dashboard from '../dashboard/dashboard'
 import BillingCycle from '../billingCycle/billingCycle'
 
 export default props => (
     <Router history={hashHistory}>
-        <Route path='/' component={Dashboard} />
-        <Route path='/billingCycles' component={BillingCycle} />
-        <Redirect from='*' to ='/' />
+        <Route path='/' component={AuthOrApp}>
+            <IndexRoute component={Dashboard} />
+            <Route path='/billingCycles' component={BillingCycle} />
+        </Route>
+        <Redirect from='*' to='/' />
     </Router>
-
 )
