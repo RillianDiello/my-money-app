86ba6092e75967ead4fad73c5961c754a075d047
Rillian Diello <rilliansf@gmail.com>
2022-01-04 23:38:52 -0400

    Removing unecessary include Change name for sideBar

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
 
 import Header from '../common/template/header'
-import SideBar from '../common/template/sidebar'
+import SideBar from '../common/template/sideBar'
 import Footer from '../common/template/footer'
-import Routes from './routes'
 import Messages from '../common/msg/messages'
 
 export default props => (
