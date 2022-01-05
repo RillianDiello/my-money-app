30b9a1c77bc4be7386be1c1af1a13b789331aa17
Rillian Diello <rilliansf@gmail.com>
2022-01-05 00:16:21 -0400

    Change imports chanege credits and debts for singular

--- frontend/src/billingCycle/summary.jsx
+++ frontend/src/billingCycle/summary.jsx
@@ -1,23 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 import Grid from '../common/layout/grid'
 import Row from '../common/layout/row'
 import ValueBox from '../common/widget/valueBox'
 
-export default ({credits, debts}) => (
+export default ({credit, debt}) => (
     <Grid cols='12'>
         <fieldset>
-            <legend>Resume</legend>
+            <legend>Resumo</legend>
             <Row>
                 <ValueBox cols='12 4' color='green' icon='bank'
-                    value={`R$ ${credits}`} text='Credits Amount' />
-                <ValueBox cols='12 4' color='red' icon='bank'
-                    value={`R$ ${debts}`} text='Debits Amount' />
-                <ValueBox cols='12 4' color='blue' icon='bank'
-                    value={`R$ ${credits - debts}`} text='Consolidated Amount' />
-
+                    value={`R$ ${credit}`} text='Total de Créditos' />
+                <ValueBox cols='12 4' color='red' icon='credit-card'
+                    value={`R$ ${debt}`} text='Total de Débitos' />
+                <ValueBox cols='12 4' color='blue' icon='money'
+                    value={`R$ ${credit - debt}`} text='Valor Consolidado' />
             </Row>
         </fieldset>
     </Grid>
 )
-
