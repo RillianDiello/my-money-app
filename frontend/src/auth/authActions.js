b3c91f64419893a6610771a18061ac04dde7f048
Rillian Diello <rilliansf@gmail.com>
2022-01-04 23:46:03 -0400

    Eslint for grid Eslint for custon.css Rename import const in authActions

--- frontend/src/auth/authActions.js
+++ frontend/src/auth/authActions.js
@@ -1,6 +1,6 @@
 import { toastr } from 'react-redux-toastr'
 import axios from 'axios'
-import consts from '../const.s'
+import consts from '../consts'
 
 export function login(values) {
     return submit(values, `${consts.OAPI_URL}/login`)
