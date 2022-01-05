b8e5ae6978d4d57fae6bed1939aba6e4c055cbba
Rillian Diello <rilliansf@gmail.com>
2022-01-04 23:38:19 -0400

    Removing comments and / for Routing

--- frontend/src/main/routes.jsx
+++ frontend/src/main/routes.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
-import { Router, Route, Redirect, hashHistory, IndexRoute } from 'react-router'
+import { Router, Route, IndexRoute, Redirect, hashHistory } from 'react-router'
 
-// import Dashboard from '../dashboard/dashboard'
 import AuthOrApp from './authOrApp'
 import Dashboard from '../dashboard/dashboard'
 import BillingCycle from '../billingCycle/billingCycle'
@@ -10,7 +9,7 @@ export default props => (
     <Router history={hashHistory}>
         <Route path='/' component={AuthOrApp}>
             <IndexRoute component={Dashboard} />
-            <Route path='/billingCycles' component={BillingCycle} />
+            <Route path='billingCycles' component={BillingCycle} />
         </Route>
         <Redirect from='*' to='/' />
     </Router>
